04ad273b0c7ba5f982826bfc2fc4c7bed0a1ffa4
Victoria Mak <33503617+viymak@users.noreply.github.com>
2020-02-14 21:29:24 -0500

    Update reproject-points.py

--- reproject-points.py
+++ reproject-points.py
@@ -5,9 +5,11 @@ from shapely.geometry import Point
 import fiona
 from os import listdir
 
+# user input - the user must rename the file
 replace_string = input('You will need to rename these files. Which part of the filename should be replaced?\n')
 
 new_filename = input('What would you like it replaced with?\n')
+
 
 for text_file in listdir('.'):
     if text_file.endswith('.py') | text_file.startswith('.'):
@@ -22,18 +24,23 @@ for text_file in listdir('.'):
 
         geo_df = GeoDataFrame(gcps, crs=crs, geometry=geometry)
 
+        #for debugging:
         #print(geo_df.crs)
 
+        # convert coordinates to epsg:4326
         points_4326 = geo_df.to_crs({'init': 'epsg:4326'})
-
-        #print(points_4326.geometry[0].x)
 
         points_4326.mapX = [point.x for point in points_4326.geometry]
         points_4326.mapY = [point.y for point in points_4326.geometry]
         points_4326.enable = points_4326['enable'].astype(int)
 
-        #print(points_4326)
-        #data = pd.DataFrame(mapX: points_4326.map)
+        # convert to pandas dataframe (exporting a csv with geodataframe created a new directory for each file)
+        # geometry column is dropped to match original formatting
+        pandas_data = pd.DataFrame(points_4326).drop(['geometry'], axis=1)
+        #(mapX: points_4326.map)
+
+        #for debugging:
+        #print(pandas_data)
 
         new_fn = text_file.replace(replace_string, new_filename)
-        points_4326.to_file(new_fn, driver="CSV")
+        pandas_data.to_csv(new_fn)
